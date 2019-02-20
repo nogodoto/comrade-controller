7a17d5fff82aaf69a243c61f8df953aea15fa8d6
nogodoto <nogoux@gmail.com>
2019-02-20 15:49:34 -0400

    Sample RESTful API using ASP.NET

---
+++ RESTAPI/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTAPI.Models;
+using RESTAPI.Services;
+using System.Web.Http;
+using System.Net.Http;
+
+
+
+namespace RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            return response;
+
+        }
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
